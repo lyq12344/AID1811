946ed035666b7891c051010fff2826e25e2920c5
Lyq <lyq@tedu.cn>
2019-01-30 11:49:43 +0800

    first commit

---
+++ regex.py
@@ -0,0 +1,22 @@
+import re
+
+pattern = r"(\w+):(\d+)"
+s = "张:1994,李:1995"
+
+#直接用re 调用
+# l = re.findall(pattern, s)
+# print(l)
+
+#compile对象调用
+regex = re.compile(pattern)
+l = regex.findall(s, pos=0,endpos=8)
+print(l)
+
+l = re.split(r'\s+',"Hello    world nihao Kitty")
+print(l)
+
+s = re.sub(r'\s+', '##', "Hello    world nihao Kitty", 2)
+print(s)
+
+s = re.subn(r'\s+', '##', "Hello    world nihao Kitty")
+print(s)
