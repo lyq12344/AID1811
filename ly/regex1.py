05f2722861903eb0c546353033c4c2f982a0dafd
Lyq <lyq@tedu.cn>
2019-01-30 12:57:24 +0800

    remove

---
+++ ly/regex1.py
@@ -0,0 +1,27 @@
+import re
+
+pattern = r'\d+'
+s = "2018年中国经济增长6.6%,与2017年基本持平,2019年面临很多困难"
+
+it = re.finditer(pattern, s)
+
+#math对象属性
+# print(dir(next(it)))
+
+# for i in it:
+    # print(i)
+
+# for i in it:
+#     print(i.group())
+
+try:
+    obj = re.fullmatch(r'\w+',"hello1973")
+    print(obj.group())
+except AttributeError as e:
+    print(e)
+
+obj = re.match(r'[A-Z]\w+',"Hello world")
+print(obj.group())
+
+obj = re.search(r'\d+', s)
+print(obj.group())
